bb227db752eaf9341ee3e3fa2f1a7aec86878f2b
crandino <carlosmanbe@hotmail.com>
2015-05-29 11:44:19 +0200

    DynArray with tests
    Tests have not been checked. New method insertArray.

--- UnitTest_finalExam/unittest1.cpp
+++ UnitTest_finalExam/unittest1.cpp
@@ -1,9 +1,11 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include <time.h>
 #include "../Point2d.h"
 #include "../String.h"
 #include "../String.cpp"
 #include "../DList.h"
+#include "../DynArray.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,7 +14,7 @@ namespace UnitTest_finalExam
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
+
 		// ---------------------------------------------
 		// --------------- Point2d ---------------------
 		// ---------------------------------------------
@@ -111,7 +113,7 @@ namespace UnitTest_finalExam
 		TEST_METHOD(Point2dIsClosedTo)
 		{
 			Point2d<int> p1, p2;
-			p1.setZero(); 
+			p1.setZero();
 			p2.x = 2;
 			p2.y = 2;
 
@@ -312,7 +314,7 @@ namespace UnitTest_finalExam
 			Assert::IsTrue(dl1.getNodeAtPos(1)->data == 2);
 			Assert::IsTrue(dl1.getNodeAtPos(2)->data == 3);
 			Assert::IsTrue(dl1.getNodeAtPos(3)->data == 4);
-			
+
 		}
 		TEST_METHOD(DListCount)
 		{
@@ -453,6 +455,199 @@ namespace UnitTest_finalExam
 		for (unsigned int i = 0; i < d.count() - 1; i++)
 		Assert::IsTrue(d[i] <= d[i + 1]);
 		}*/
-		
+
+		// ---------------------------------------------
+		// ---------------- DynArray--------------------
+		// ---------------------------------------------
+		TEST_METHOD(DynArrayConstruct1)
+		{
+			DynArray<int> dyn1;
+
+			Assert::IsTrue(dyn1.getMemory() == 16);
+			Assert::IsTrue(dyn1.getNumElements() == 0);
+		}
+		TEST_METHOD(DynArrayConstruct2)
+		{
+			DynArray<int> dyn1(5);
+
+			Assert::IsTrue(dyn1.getMemory() == 5);
+			Assert::IsTrue(dyn1.getNumElements() == 0);
+		}
+		TEST_METHOD(DynArrayPushBack)
+		{
+			DynArray<int> dyn1;
+			dyn1.pushBack(13);
+			Assert::IsTrue(dyn1.getMemory() == 16);
+			Assert::IsTrue(dyn1.getNumElements() == 1);
+
+			DynArray<double> dyn2(1);
+			dyn2.pushBack(13.99823);
+			dyn2.pushBack(13.29234);
+			Assert::IsTrue(dyn2.getMemory() == 2);
+			Assert::IsTrue(dyn2.getNumElements() == 2);
+
+			DynArray<double> dyn3(4);
+			dyn3.pushBack(13.99823);
+			dyn3.pushBack(13.29234);
+			Assert::IsTrue(dyn3.getMemory() == 4);
+			Assert::IsTrue(dyn3.getNumElements() == 2);
+		}
+		TEST_METHOD(DynArrayPop)
+		{
+			int a;
+			DynArray<int> dyn1;
+			dyn1.pushBack(13);
+			dyn1.pop(a);
+			Assert::IsTrue(dyn1.getMemory() == 16);
+			Assert::IsTrue(dyn1.getNumElements() == 0);
+			dyn1.pop(a);
+			Assert::IsTrue(dyn1.getMemory() == 16);
+			Assert::IsTrue(dyn1.getNumElements() == 0);
+
+			float b;
+			DynArray<float> dyn2;
+			Assert::IsFalse(dyn2.pop(b));
+			Assert::IsTrue(dyn2.getMemory() == 16);
+			Assert::IsTrue(dyn2.getNumElements() == 0);
+
+			DynArray<float> dyn3(6);
+			dyn3.pushBack(3);
+			dyn3.pushBack(4);
+			dyn3.pushBack(5);
+			Assert::IsTrue(dyn3.pop(b));
+			Assert::IsTrue(dyn3.getMemory() == 6);
+			Assert::IsTrue(dyn3.getNumElements() == 2);
+		}
+		TEST_METHOD(DynArrayInsert)
+		{
+			DynArray<int> dyn1;
+			Assert::IsTrue(dyn1.insert(13, 1) == false);
+			Assert::IsTrue(dyn1.getMemory() == 16);
+			Assert::IsTrue(dyn1.getNumElements() == 0);
+			Assert::IsTrue(dyn1.insert(12, 0) == true);
+			Assert::IsTrue(dyn1.getMemory() == 16);
+			Assert::IsTrue(dyn1.getNumElements() == 1);
+			dyn1.pushBack(11);
+			dyn1.pushBack(12);
+			dyn1.pushBack(13);
+			Assert::IsTrue(dyn1.insert(12, 0) == true);
+			Assert::IsTrue(dyn1.insert(12, 2) == true);
+			Assert::IsTrue(dyn1.getMemory() == 16);
+			Assert::IsTrue(dyn1.getNumElements() == 6);
+		}
+
+		TEST_METHOD(DynArrayInsertAray)
+		{
+			DynArray<int> dyn1;
+			 
+			dyn1.pushBack(1);
+			dyn1.pushBack(2);
+			dyn1.pushBack(3);
+			dyn1.pushBack(4);
+			dyn1.pushBack(5);
+
+			DynArray<int> dyn2;
+
+			dyn2.pushBack(991);
+			dyn2.pushBack(992);
+			dyn2.pushBack(993);
+			dyn2.pushBack(994);
+			dyn2.pushBack(995);
+			
+			dyn1.insert(dyn2, 2);
+			
+			Assert::AreEqual((int)dyn1[0], 1);
+			Assert::AreEqual((int)dyn1[1], 2);
+			Assert::AreEqual((int)dyn1[2], 991);
+			Assert::AreEqual((int)dyn1[3], 992);
+			Assert::AreEqual((int)dyn1[4], 993);
+			Assert::AreEqual((int)dyn1[5], 994);
+			Assert::AreEqual((int)dyn1[6], 995);
+			Assert::AreEqual((int)dyn1[7], 3);
+			Assert::AreEqual((int)dyn1[8], 4);
+			Assert::AreEqual((int)dyn1[9], 5);
+		}
+		TEST_METHOD(DynArrayOperatorClaudator)
+		{
+			DynArray<int> dyn1;
+			dyn1.pushBack(8);
+			Assert::IsTrue(dyn1[0] == 8);
+		}
+
+		TEST_METHOD(DynArrayFlip)
+		{
+			DynArray<unsigned int> dyn1;
+			dyn1.pushBack(1);
+			dyn1.pushBack(2);
+			dyn1.pushBack(3);
+			dyn1.pushBack(4);
+			dyn1.pushBack(5);
+			dyn1.pushBack(6);
+			dyn1.pushBack(7);
+
+			dyn1.flip();
+
+			for (unsigned int i = 0, j = 7; i < dyn1.getNumElements(); i++, j--)
+				Assert::AreEqual(dyn1[i], j);
+		}
+
+		TEST_METHOD(TestSorting)
+		{
+			DynArray<int> test(5);
+			DynArray<int> solution(5);
+
+			test.pushBack(5);
+			test.pushBack(4);
+			test.pushBack(10);
+			test.pushBack(8);
+			test.pushBack(3);
+
+			solution.pushBack(3);
+			solution.pushBack(4);
+			solution.pushBack(5);
+			solution.pushBack(8);
+			solution.pushBack(10);
+
+			test.sort();
+
+			for (unsigned int i = 0; i < test.getNumElements(); i++)
+				Assert::AreEqual(test[i], solution[i]);
+
+			DynArray<int> d;
+
+			d.pushBack(5);
+			d.pushBack(4);
+			d.pushBack(10);
+			d.pushBack(8);
+			d.pushBack(3);
+			d.pushBack(3);
+			d.pushBack(-63);
+			d.pushBack(22);
+			d.pushBack(0);
+			d.pushBack(16);
+			d.pushBack(-9);
+			d.pushBack(-6);
+			d.pushBack(15);
+			d.pushBack(1);
+			d.pushBack(0);
+
+			d.sort();
+
+			for (unsigned int i = 0; i < d.getNumElements() - 1; i++)
+				Assert::IsTrue(d[i] <= d[i + 1]);
+
+			// ---- Bubble optimized ----
+
+			DynArray<int> d_opt;
+			srand(time(NULL));
+
+			for (unsigned int i = 0; i < 10000; i++)
+				d_opt.pushBack(rand());
+
+			d_opt.sort_opt();
+
+			for (unsigned int i = 0; i < d_opt.getNumElements() - 1; i++)
+				Assert::IsTrue(d_opt[i] <= d_opt[i + 1]);
+		}
 	};
 }
